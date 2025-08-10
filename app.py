432a5e80ba8f63a16bcd09ad7e6467b4022b805c
Aku <mindstormakash@gmail.com>
2025-08-10 10:50:54 -0700

    s3 implementation

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 from PIL import Image
 import cv2
 import torch
-from functions import ocr_extraction
-
+from functions import ocr_extraction, save_image_s3
+import numpy as np
 UPLOAD_DIR = "uploaded_images_test"
 
 st.title("License Plate Detection")
@@ -18,32 +18,30 @@ with st.form(key="license"):
         if uploaded_file is None:
             st.warning("Please upload a file before submitting.")
         else:
-            if not os.path.exists(UPLOAD_DIR):
-                os.makedirs(UPLOAD_DIR)
+            model = YOLO("best.pt")
+            image_bytes = uploaded_file.read()
+            np_arr = np.frombuffer(image_bytes, np.uint8)
+            image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            results = model(image)
+            
+            #plot image on streamlit
+            res_plotted = results[0].plot()
+            st.image(res_plotted, caption="Detection Result", use_container_width=True)
+ 
+            #get the coordinates    
+            plate_coord = results[0].boxes.data.tolist()
+            coord = [plate_coord[0][0], plate_coord[0][1], plate_coord[0][2], plate_coord[0][3]]
+            x1, y1, x2, y2 = map(int,coord)
 
-            file_name = uploaded_file.name
-            save_path = os.path.join(UPLOAD_DIR, file_name)
 
-            if os.path.exists(save_path):
-                st.error(f"A file named '{file_name}' already exists.")
-            else:
-                with open(save_path, "wb") as f:
-                    f.write(uploaded_file.getbuffer())
-                st.success(f"File '{file_name}' uploaded and saved successfully.")
+            #license cropped
+            plate_img = image[y1:y2, x1:x2]
+            plate_img_rgb = cv2.cvtColor(plate_img, cv2.COLOR_BGR2RGB) #to display on streamlit
+            st.image(plate_img_rgb)
 
-                # Run inference after saving
-                model = YOLO("best.pt")
-                results = model(save_path)
+            #ocr extraction
+            license_number = ocr_extraction(plate_img)
+            st.write("License Tracker: ",license_number)
 
-                # Get image with detections plotted 
-                res_plotted = results[0].plot()
-                plate_coord = results[0].boxes.data
-                python_list = plate_coord.tolist()
-                coord = [python_list[0][0], python_list[0][1], python_list[0][2], python_list[0][3]]
-                x1, y1, x2, y2 = map(int,coord)
-                st.image(res_plotted, caption="Detection Result", use_container_width=True)
-                image = cv2.imread(save_path)
-                plate_img = image[y1:y2, x1:x2]
-                plate_img_rgb = cv2.cvtColor(plate_img, cv2.COLOR_BGR2RGB) #to display on streamlit
-                st.image(plate_img_rgb)
-                st.write("License Tracker: ",ocr_extraction(plate_img))
+            presigned_url = save_image_s3(res_plotted, license_number)
+            st.write("View Detection Image Link: ", presigned_url)
--- functions.py
+++ functions.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 import boto3
 import cv2
 import os
-
+from datetime import datetime
+from PIL import Image
+import io
 load_dotenv()
 
 access_key = os.getenv("AWS_ACCESS_KEY_ID")
@@ -14,6 +16,12 @@ textract_client = boto3.client("textract",
                             aws_access_key_id=access_key,
                             aws_secret_access_key=secret_key,
                             region_name=region)
+
+
+s3_client = boto3.client("s3",
+                        aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key,
+                        region_name=region)
 
 
 def ocr_extraction(image):
@@ -32,3 +40,16 @@ def ocr_extraction(image):
         if block["BlockType"] == "LINE":
             final_result += block["Text"]
     return final_result
+
+def save_image_s3(res_plotted, plate_number):
+    timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    full_image_pil = Image.fromarray(cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB))
+    buffer = io.BytesIO()
+    full_image_pil.save(buffer, format="JPEG")
+    buffer.seek(0)
+    key = f"detections/{plate_number}_{timestamp}.jpg"
+    s3_client.upload_fileobj(buffer, "licensetrackerbucket", key)
+    presigned_url = s3_client.generate_presigned_url(
+        'get_object', Params = {'Bucket': 'licensetrackerbucket', 'Key':key}, ExpiresIn=3600
+    )
+    return presigned_url
